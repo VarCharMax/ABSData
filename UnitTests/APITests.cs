baac592cf62756616818cee5253bb733608b75bc
Rohan Parkes <rparkes@email.com>
2023-07-12 23:35:25 +1000

    Added Tests

--- UnitTests/APITests.cs
+++ UnitTests/APITests.cs
@@ -3,9 +3,6 @@ using ABSDataFramework.Interfaces;
 using ABSDataFramework.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,20 +13,68 @@ namespace UnitTests
         [Fact]
         public async Task APIReturnsDataAsync()
         {
+            // Create mock for service dependency
             var mockService = new Mock<IABSDataService>();
 
+            //This is how to mock an asynchronous method
             var myType = mockService.Setup(p => p.GetDataByRegionIdAndSexIdAsync(104, 1)).Returns(Task.FromResult(new PopulationData { RegionCode = 104, RegionName = "West Coast" }));
 
+            //Testing /api/age-structure/104/1 endpoint.
             var controller = new AbsDataController(mockService.Object);
 
+            //Test that api call returns a successful ActionResult
             var model = await controller.GetData(104, 1) as OkObjectResult;
             
             Assert.NotNull(model);
 
+            //Test that object embeded in ActionResult is correct type.
             var result = model.Value as PopulationData;
 
             Assert.NotNull(result);
         }
 
+        [Fact]
+        public async Task APIReturnsValidObject()
+        {
+            // Create mock for service dependency
+            var mockService = new Mock<IABSDataService>();
+
+            //This is how to mock an asynchronous method
+            var myType = mockService.Setup(p => p.GetDataByRegionIdAndSexIdAsync(104, 1)).Returns(Task.FromResult(new PopulationData { RegionCode = 104, RegionName = "West Coast" }));
+
+            //Testing /api/age-structure/104/1 endpoint.
+            var controller = new AbsDataController(mockService.Object);
+
+            //Test that api call returns a successful ActionResult
+            var model = await controller.GetData(104, 1) as OkObjectResult;
+
+            //Test that object embeded in ActionResult is correct type.
+            var result = model.Value as PopulationData;
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task APIReturnsValidData()
+        {
+            // Create mock for service dependency
+            var mockService = new Mock<IABSDataService>();
+
+            //This is how to mock an asynchronous method
+            var myType = mockService.Setup(p => p.GetDataByRegionIdAndSexIdAsync(104, 1)).Returns(Task.FromResult(new PopulationData { RegionCode = 104, RegionName = "West Coast" }));
+
+            //Testing /api/age-structure/104/1 endpoint.
+            var controller = new AbsDataController(mockService.Object);
+
+            //Test that api call returns a successful ActionResult
+            var model = await controller.GetData(104, 1) as OkObjectResult;
+
+            Assert.NotNull(model);
+
+            //Test that object embeded in ActionResult is correct type.
+            var result = model.Value as PopulationData;
+
+            Assert.Equal("West Coast", result.RegionName);
+        }
     }
 }
